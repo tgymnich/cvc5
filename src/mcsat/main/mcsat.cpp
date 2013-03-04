d1bc888c36695c10d376c33a7d4460d5e7ae2925
Dejan Jovanović <dejan@cs.nyu.edu>
2013-03-04 13:17:00 -0500

    making public interface

--- src/mcsat/main/mcsat.cpp
+++ src/mcsat/main/mcsat.cpp
@@ -8,14 +8,11 @@
 #include "util/language.h"
 #include "expr/expr_manager.h"
 #include "expr/command.h"
-#include "context/context.h"
-#include "smt/smt_engine.h"
-#include "smt/smt_engine_scope.h"
 
 using namespace CVC4;
 using namespace CVC4::parser;
 
-#include "mcsat/solver.h"
+#include "mcsat/mcsat.h"
 
 using namespace std;
 
@@ -34,22 +31,13 @@ int main(int argc, char* argv[]) {
   // Solve the problems 
   for (unsigned i = 0; i < filenames.size(); ++ i) {
     try {
-      // Make an smt engine so just it has statistics registry
-      SmtEngine smtEngine(&exprManager);
-   
+
       // Create the parser
       ParserBuilder parserBuilder(&exprManager, filenames[i], options);
       Parser* parser = parserBuilder.withInputLanguage(language::input::LANG_SMTLIB_V2).build();
   
-      // Create the mcsat solver
-      context::UserContext userContext;
-      context::Context searchContext;
-
-      mcsat::Solver* mcSolver = 0;
-      { 
-        smt::SmtScope smtScope(&smtEngine);
-        mcSolver = new mcsat::Solver(&userContext, &searchContext);
-      }
+      // Create the MCSAT engine
+      mcsat::MCSatEngine mcSolver(&exprManager);
       
       // Parse and assert
       Command* cmd;
@@ -57,34 +45,20 @@ int main(int argc, char* argv[]) {
   
         AssertCommand* assert = dynamic_cast<AssertCommand*>(cmd);
         if (assert) {
-          // Get the assertion
-          Node assertion = assert->getExpr();
           // Assert to mcsat
-          {
-            smt::SmtScope smtScope(&smtEngine);
-            mcSolver->addAssertion(assertion, true);
-          }
+          mcSolver.addAssertion(assert->getExpr(), true);
         }
   
         delete cmd;
       }
   
       // Check the problem
-      bool result;
-      {
-        smt::SmtScope smtScope(&smtEngine);
-        result = mcSolver->check();
-      }
-    
+      bool result = mcSolver.check();
       cout << (result ? "sat" : "unsat") << endl;
       
       // Get rid of the parser
       delete parser;
-      // Get rid of mcSat
-      { 
-        smt::SmtScope smtScope(&smtEngine);
-        delete mcSolver;
-      }
+
     } catch (const Exception& e) {
       std::cerr << e << std::endl;
     }
