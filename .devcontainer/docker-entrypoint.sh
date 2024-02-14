072d1b8b1def0a2d35dceb8b3804be5fb82fc8df
Tim Gymnich <tgymnich@icloud.com>
2024-02-14 12:33:51 +0000

    improve devcontainer

--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -4,8 +4,6 @@ cd /workspaces/cvc5
 
 bash ./autogen.sh
 
-bash ./contrib/get-antlr-3.4
+bash ./configure --with-antlr-dir=/app/antlr/antlr-3.4 ANTLR=/app/antlr/antlr-3.4/bin/antlr3
 
-bash ./configure --with-antlr-dir=/workspaces/cvc5/antlr-3.4 ANTLR=/workspaces/cvc5/antlr-3.4/bin/antlr3
-
-make -j `nproc`
+# make -j `nproc`
